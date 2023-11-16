1926d108d01163ffc61397da5468e8ad334b5d35
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-16 10:04:14 +0100

    finished sin and tan function, added approx

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -10,7 +10,13 @@ def cmd_group():
 
 
 @cmd_group.command()
-@click.option("-n", "--number", default=10)
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of steps between 0 and 2 pi to calculate sine",
+    show_default=True,
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
@@ -19,12 +25,33 @@ def sin(number):
 
 
 @cmd_group.command()
-@click.option("-n", "--number", default=10)
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of steps between 0 and 2 pi to calculate tangent",
+    show_default=True,
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
+
+
+@cmd_group.command()
+@click.argument("error")
+def approx(error):
+    for number in np.arange(0, 2 * pi, 0.001):
+        delta = abs(number - np.sin(number))
+        if delta > float(error):
+            print(
+                f"For an accuracy of {error}, the small-angle approximation holds\nup to x = {number}"
+            )
+            return
+    print(
+        f"The given accuracy of {error} is so large that the small-angle approximation holds\nfor all values between 0 and 2 pi"
+    )
 
 
 if __name__ == "__main__":
