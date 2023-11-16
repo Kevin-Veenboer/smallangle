42177a2135f8a217d8b85e00053a9fb8c11d6f01
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-16 10:20:39 +0100

    added docstrings and comments

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -18,6 +18,11 @@ def cmd_group():
     show_default=True,
 )
 def sin(number):
+    """Function that shows values of sine between 0 and 2 pi
+
+    Args:
+        number (int): number of steps between 0 and 2 pi to calculate values of sine
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -33,6 +38,11 @@ def sin(number):
     show_default=True,
 )
 def tan(number):
+    """Function that shows values of tangent between 0 and 2 pi
+
+    Args:
+        number (int): number of steps between 0 and 2 pi to calculate values of tangent
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
@@ -42,9 +52,14 @@ def tan(number):
 @cmd_group.command()
 @click.argument("error")
 def approx(error):
+    """Function that gives the maximum value at which the small-angle approximation holds
+
+    Args:
+        error (float): the required accuracy for which the approximation has to hold
+    """
     for number in np.arange(0, 2 * pi, 0.001):
-        delta = abs(number - np.sin(number))
-        if delta > float(error):
+        # check the difference, if it is greater than the required accuracy we give the maximum value
+        if abs(number - np.sin(number)) > float(error):
             print(
                 f"For an accuracy of {error}, the small-angle approximation holds\nup to x = {number}"
             )
