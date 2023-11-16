7a02e34f2e4a25939e2653b6a9eb236b4a7e5901
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-16 09:41:46 +0100

    created smallangle package and repo

---
+++ src/smallangle/smallangle.py
@@ -0,0 +1,31 @@
+import click
+import numpy as np
+from numpy import pi
+import pandas as pd
+
+
+@click.group()
+def cmd_group():
+    pass
+
+
+@cmd_group.command()
+@click.option("-n", "--number", default=10)
+def sin(number):
+    x = np.linspace(0, 2 * pi, number)
+    df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
+    print(df)
+    return
+
+
+@cmd_group.command()
+@click.option("-n", "--number", default=10)
+def tan(number):
+    x = np.linspace(0, 2 * pi, number)
+    df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
+    print(df)
+    return
+
+
+if __name__ == "__main__":
+    cmd_group()
